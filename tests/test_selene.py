d9e12d8893a99b80133e88e843fdba40de568649
Egor Ivanov <cheshi.mantu@pm.me>
2022-10-03 21:23:10 +0300

    updates

--- tests/test_selene.py
+++ tests/test_selene.py
@@ -8,25 +8,10 @@ def test_github(setup_browser):
     browser.open("https://github.com/")
 
     s('.header-search-input').click()
-    s('.header-search-input').send_keys('cheshi-mantu/playwright-example')
+    s('.header-search-input').send_keys('cheshi-mantu/')
     s('.header-search-input').submit()
 
-    s(by.link_text("cheshi-mantu/playwright-example")).click()
-
-    s("#issues-tab").click()
-
-    s(by.partial_text("#1")).should(be.visible)
-
-
-def test_github_new(setup_browser):
-    browser = setup_browser
-    browser.open("https://github.com/")
-
-    s('.header-search-input').click()
-    s('.header-search-input').send_keys('cheshi-mantu/playwright-example')
-    s('.header-search-input').submit()
-
-    s(by.link_text("cheshi-mantu/playwright-example")).click()
+    s(by.link_text("cheshi-mantu/qa-local-test-bed")).click()
 
     s("#issues-tab").click()
 
