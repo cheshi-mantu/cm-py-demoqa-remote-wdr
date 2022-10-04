172888654fe91685957b7a381a66a98c72a6519b
Egor Ivanov <cheshi.mantu@pm.me>
2022-10-04 21:55:41 +0300

    baseurl

--- demoqa_test/controls/utils.py
+++ demoqa_test/controls/utils.py
@@ -17,6 +17,6 @@ def resource(relative_path):
 
 
 def arrange_form_opened():
-    browser.open('https://demoqa.com/automation-practice-form')
+    browser.open('/automation-practice-form')
     browser.all(
         '[id^=google_ads][id$=container__]').with_(timeout=10).should(have.size(3)).perform(command.js.remove)
